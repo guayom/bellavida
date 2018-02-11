3fa85f082c5047f896c57a33c62ee9a2a6a5f753
guayom <guayo.mena@gmail.com>
2018-02-10 19:42:42 -0600

    Give very basic styling to menu

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -1,32 +1,105 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from "styled-components"
 
-const Test = () => (
-  <div>Hello</div>
-)
+const MenuContainer = styled.div`
+  background: lightGrey;
+  z-index: 10;
+`
+
+const List = styled.ul`
+  list-style:none;
+  margin: 0;
+  padding: 0;
+`
+
+const Item = styled.li`
+  display: inline-block;
+  position:relative;
+  margin: 0;
+  padding: 10px;
+  padding-right: ${props => props.hasChildren ? '15px' : '0'};
+
+  &:after{
+    display: ${props => props.hasChildren ? 'block' : 'none'};
+    position:absolute;
+    right: 0;
+    top: 50%;
+    margin-top: -2px;
+    content: "";
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 4px 0 4px 5px;
+    border-color: transparent transparent transparent black;
+    transition: all .1s ease;
+  }
+
+  &:hover:after {
+    transform: rotate(90deg);
+  }
+
+  &:hover .SubmenuContainer {
+    display: flex;
+  }
+`
+
+const SubmenuContainer = styled.div`
+  position:absolute;
+  top: 100%;
+  left: 0;
+  background: lightGrey;
+  padding: 20px;
+  display: none;
+  justify-content: space-between;
+
+  li {
+    display:block;
+  }
+`
+
+const SubmenuColumn = styled.div`
+  margin-right: 20px;
+  width: 180px;
+  &:last-of-type {
+    margin-right: 0;
+  }
+`
+
+const SubMenuTitle = styled.h3`
+  display: block;
+  font-size: 14px;
+  margin: 0;
+`
 
 const SubMenu = (props) => (
-  <div>
-    <h3>{props.title}</h3>
-    <ItemsList items={props.pages} />
-  </div>
+  <SubmenuContainer className="SubmenuContainer">
+    {props.items.map(function(submenu){
+      return(
+        <SubmenuColumn key={submenu.id}>
+          <SubMenuTitle>{submenu.title}</SubMenuTitle>
+          <ItemsList items={submenu.items} />
+        </SubmenuColumn>
+      )
+    })}
+  </SubmenuContainer>
 )
 
 const ListItem = (props) => (
-  <li key={props.item.id}>
+  <Item key={props.item.id} hasChildren={props.item.children}>
     {props.item.title}
-    {props.item.children ? props.item.children.map(c => <SubMenu key={c.title} pages={c.items} title={c.title} />) : null}
-  </li>
+    {props.item.children ? <SubMenu items={props.item.children} /> : null}
+  </Item>
 )
 
 function ItemsList(props) {
   const items = props.items;
   return (
-    <ul>
+    <List>
       {items.map(function(item){
         return (<ListItem item={item} key={item.id} />)
       })}
-    </ul>
+    </List>
   );
 }
 
@@ -132,9 +205,9 @@ class Menu extends React.Component {
     const pages = MenuItems.map(p => p[locale])
 
     return (
-      <div>
+      <MenuContainer>
         <ItemsList items={pages} />
-      </div>
+      </MenuContainer>
     )
   }
 }
