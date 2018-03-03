617ed484be5a4ee53a1ce6826c71ade999080fa3
guayom <guayo.mena@gmail.com>
2018-03-03 14:00:36 -0600

    Style features for desktop

--- src/components/Features/index.js
+++ src/components/Features/index.js
@@ -1,18 +1,37 @@
 import React from "react"
+import styled from 'styled-components'
+
+const FeaturesList = styled.ul`
+  display: block;
+  padding: 0 0 60px;
+  margin: 0;
+  display: flex;
+`
+
+const FeatureItem = styled.li`
+  display: block;
+  flex: 1 0;
+  text-align: center;
+`
+
+const ImageContainer = styled.div`
+  margin: 0 0 20px;
+`
 
 class Features extends React.Component {
   render() {
     const featureItems = this.props.items.map((feature, index) =>
-      <li key={feature.node.id}>
-        <img src={feature.node.image.resolutions.src} alt={feature.node.title}/>
-        {feature.node.title}
-        {feature.node.content.content}
-      </li>
+      <FeatureItem key={feature.node.id}>
+        <ImageContainer>
+          <img src={feature.node.image.resolutions.src} alt={feature.node.title}/>
+        </ImageContainer>
+        <h3>{feature.node.title}</h3>
+        <p>{feature.node.content.content}</p>
+      </FeatureItem>
     );
     return(
       <div>
-        <h2>Features</h2>
-        <ul>{featureItems}</ul>
+        <FeaturesList>{featureItems}</FeaturesList>
       </div>
     )
   }
