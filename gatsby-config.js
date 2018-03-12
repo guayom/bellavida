09a16c5221b43c4c631d9ff32a573c580513669b
guayom <guayo.mena@gmail.com>
2018-03-12 00:08:59 -0600

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,24 @@ module.exports = {
         accessToken: process.env.ACCESS_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/images/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: true,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
     'gatsby-plugin-react-helmet',
     `gatsby-transformer-remark`,
     `gatsby-plugin-styled-components`,
