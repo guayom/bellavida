1664f4062289632770662bfad36acf5b166709d4
guayom <guayo.mena@gmail.com>
2018-03-14 06:24:00 -0600

    Mobile styled for Wrapper

--- src/components/Layout/Wrapper.js
+++ src/components/Layout/Wrapper.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
+import breakpoint from 'styled-components-breakpoint'
 
 const Wrapper = styled.div`
-  padding-left: 80px;
-  padding-right: 80px;
+  padding-left: 20px;
+  padding-right: 20px;
   box-sizing:border-box;
   margin: 0 auto;
+
+  ${breakpoint('tablet') `
+    padding-left: 80px;
+    padding-right: 80px;
+  `}
 `
 
 export default Wrapper
