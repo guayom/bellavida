43b93a645d8d6297626970bf481605253029cdf4
guayom <guayo.mena@gmail.com>
2018-03-13 23:24:48 -0600

    Add form to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -30,11 +30,11 @@ const FooterWrapper = Wrapper.extend`
 `
 
 const Column =  styled.div`
-  flex-grow: ${props => props.first ? '2' : '1'};
+  flex-grow: ${props => props.first ? '1.5' : '1'};
   flex-basis: 0;
   border-right: ${props => props.first ? `1px solid rgba(255,255,255,0.05)` : 'none'};
   padding-left: ${props => props.first ? '0' : '40px'};
-  padding-right: 40px;
+  padding-right: ${props => props.first ? '40px' : '0'};
 `
 
 const Title = styled.h3`
@@ -42,11 +42,75 @@ const Title = styled.h3`
   font-size: 13px;
 `
 
+const Input = styled.input`
+  background: #000000;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #000000;
+  font-size: 11px;
+  box-shadow: none;
+  border-radius: 2px;
+  height: 34px;
+  padding: 6px 12px;
+  display: block;
+  line-height: 20px;
+  color: #555555;
+  transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
+  cursor: text;
+  margin-bottom: 10px;
+
+  &:hover, &:focus {
+    border-color: ${props => props.theme.mainColor};
+    outline: none;
+  }
+`
+
+const Button = styled.button`
+  background: #000000;
+  border-color: #000000;
+  font-size: 11px;
+  padding: 7px 12px;
+  transition: all 0.2s;
+  -webkit-appearance: button;
+  cursor: pointer;
+  color: #ffffff;
+  display: inline-block;
+  margin-bottom: 0;
+  font-weight: normal;
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid transparent;
+  white-space: nowrap;
+  line-height: 20px;
+  border-radius: 4px;
+  user-select: none;
+
+  &:hover {
+    background: ${props => props.theme.mainColor};
+  }
+`
+
 const Menu = ({items, prefix}) => (
-  <ul>
+  <ul style={{listStyle: `none`, padding: 0, margin: 0}}>
     {items.map(item => (
-      <li key={item.node.id}>
-        <Link to={`${prefix}${item.node.slug}`}>
+      <li 
+        key={item.node.id}
+        style={{
+          margin: 0,
+          padding: 0,
+        }}>
+        <Link 
+          to={`${prefix}${item.node.slug}`}
+          style={{
+            fontSize: `11px`,
+            color: `#595959`,
+            padding: `4px 0`,
+            display: `block`,
+          }}
+          activeStyle={{
+            color: '#93c548'
+          }}
+          >
           {item.node.title}
         </Link>
       </li>
@@ -95,6 +159,15 @@ export default ({ phoneNumbers, socialNetworks, translation, locale, brands, pro
       </Column>
       <Column>
         <Title>Get Our Newsletter</Title>
+        <form name="subscribe" method="post" data-netlify="true" data-netlify-honeypot="text">
+          <p style={{display: `none`}}>
+            <input type="hidden" name="form-name" value="subscribe" />
+            <input name="text"/>
+          </p>
+          <Input type="text" name="name" placeholder="Name"/>
+          <Input type="email" name="email" placeholder="Email"/>
+          <Button type="submit">Subscribe</Button>
+        </form>
       </Column>
     </FooterWrapper>
     <FooterWrapper subfooter>
