6501d27ebd3fecf4cbe8f31ebc1a9e7dbe9d47ec
guayom <guayo.mena@gmail.com>
2018-03-21 22:11:02 -0600

    Fix errors on location component

--- src/components/Location/index.js
+++ src/components/Location/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import TitleBar from  '../../components/General/TitleBar'
 import styled from 'styled-components'
+import breakpoint from 'styled-components-breakpoint'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 const Container = styled.div`
-    flex: 1 0;
+  flex: 1 0;
 `
 
-const latitude = 10.308094191678
-const longitude = -84.86571288339
+const latitude = 10.308094191678;
+const longitude = -84.86571288339;
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
     <GoogleMap
@@ -27,12 +28,12 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) =>
 export default (props) => (
     <Container>
         <TitleBar title="Visit our showrooms" />
-        < MyMapComponent
+        <MyMapComponent
             isMarkerShown
             googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-            loadingElement={< div style={{ height: `100%` }} />}
-            containerElement={< div style={{ height: `400px` }} />}
-            mapElement={< div style={{ height: `100%` }} />}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
         />
     </Container>
 )
