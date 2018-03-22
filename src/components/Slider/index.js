1e374d3a66b41bde60673e829230e9a01417e536
guayom <guayo.mena@gmail.com>
2018-03-21 21:45:02 -0600

    Responsive slider

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -3,28 +3,43 @@ import Img from "gatsby-image"
 import styled from 'styled-components'
 import breakpoint from 'styled-components-breakpoint'
 
-const ButtonsContainer = styled.div`
-  z-index: 10;
-  
+const Container = styled.div`
+  position: relative;
+  z-index: 1;
+  background: #000;
+  height: 85vh;
+  overflow: hidden;
 
   ${breakpoint('tablet') `
-    position: absolute;
+    height: auto;
+  `}
+`
+
+const ButtonsContainer = styled.div`
+  z-index: 10;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  ${breakpoint('tablet') `
     left: 80px;
     bottom: -50px;
+    display: block;
   `}
 `
 
 const Button = styled.button`
   display: block;
-  width: 250px;
-  height: 90px;
   background: ${props => props.active ? props.theme.mainColor : '#000'};
   color: #fff;
   border: none;
-  text-align:left;
-  padding: 0 30px;
-  border-bottom: ${props => props.active ? `solid 1px ${props.theme.mainColor}` : 'solid 1px #333'};
+  text-align:center;
+  padding: 12px;
   position:relative;
+  font-size: 11px;
 
   &:after {
     content: "";
@@ -32,18 +47,38 @@ const Button = styled.button`
     width: 0;
     height: 0;
     display:block;
-    left: 100%;
-    top: 50%;
-    margin-top: -10px;
-    border-left: ${props => props.active ? `solid 10px ${props.theme.mainColor}` : 'none'};
-    border-bottom: ${props => props.active ? `solid 10px transparent` : 'none'};
-    border-top: ${props => props.active ? `solid 10px transparent` : 'none'};
-    border-right: none;
+    left: 50%;
+    bottom: 99%;
+    margin-left: -10px;
+    border-bottom: ${props => props.active ? `solid 10px ${props.theme.mainColor}` : 'none'};
+    border-left: ${props => props.active ? `solid 10px transparent` : 'none'};
+    border-right: ${props => props.active ? `solid 10px transparent` : 'none'};
+    border-top: none;
   }
 
   &:focus {
     outline: none;
   }
+
+  ${breakpoint('tablet') `
+    width: 250px;
+    height: 90px;
+    border-bottom: ${props => props.active ? `solid 1px ${props.theme.mainColor}` : 'solid 1px #333'};
+    text-align:left;
+    font-size: 13px;
+    padding: 0 30px;
+
+    &:after {
+      left: 100%;
+      top: 50%;
+      bottom: auto;
+      margin-left: 0;
+      margin-top: -10px;
+      border-left: ${props => props.active ? `solid 10px ${props.theme.mainColor}` : 'none'};
+      border-bottom: ${props => props.active ? `solid 10px transparent` : 'none'};
+      border-top: ${props => props.active ? `solid 10px transparent` : 'none'};
+    }
+  `}
 `
 
 const SlideDescription = styled.div`
@@ -54,13 +89,17 @@ const SlideDescription = styled.div`
   right: 80px;
   background: rgba(0,0,0,0.5);
   color: #fff;
-  font-size: 40px;
+  font-size: 28px;
   z-index: 4;
   text-align:right;
   padding: 30px;
   box-sizing:border-box;
   font-weight: 100;
   font-family: 'Montserrat', sans-serif;
+
+  ${breakpoint('tablet') `
+    font-size: 40px;
+  `}
 `
 
 class Slider extends React.Component {
@@ -104,25 +143,7 @@ class Slider extends React.Component {
     var visibleSlideImage = visibleSlide.image.resolutions
 
     return(
-      <div
-        style={{
-          position: `relative`,
-          zIndex:1,
-          background: '#000'
-        }}
-        >
-
-        <ButtonsContainer>
-          {this.props.slides.map((slide, index) =>
-            <Button
-              key={slide.node.id}
-              onClick={e => this.changeSlide(index)}
-              active={index === visibleSlideIndex}
-              >
-              {slide.node.title}
-            </Button>
-          )}
-        </ButtonsContainer>
+      <Container>
 
         <SlideDescription>
           {visibleSlide.title}
@@ -150,7 +171,19 @@ class Slider extends React.Component {
           )}
         </div>
 
-      </div>
+        <ButtonsContainer>
+          {this.props.slides.map((slide, index) =>
+            <Button
+              key={slide.node.id}
+              onClick={e => this.changeSlide(index)}
+              active={index === visibleSlideIndex}
+            >
+              {slide.node.title}
+            </Button>
+          )}
+        </ButtonsContainer>
+
+      </Container>
     )
   }
 }
