6127c15adbdae10efbc541df93d452d73ae9fb89
Guayo Mena <guayo.mena@gmail.com>
2019-06-18 08:03:56 -0600

    Programatically generate pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,117 @@
+// Create Home Pages
+async function createHomePages(actions, reporter) {
+  const { createPage, createPageDependency } = actions
+  const homePages = [
+    { locale: "en", path: "/", pageTitle: "Bellavida Costa Rica" },
+    { locale: "es", path: "/es/", pageTitle: "Bellavida Costa Rica" },
+  ]
+  homePages.forEach(({ path, locale, pageTitle }) => {
+    const translation = homePages.find(p => p.locale !== locale).path
+    const id = `home-${locale}`
+    reporter.info(`Creating page: ${path} -> ${translation}`)
+
+    createPage({
+      path,
+      component: require.resolve("./src/templates/home.js"),
+      context: { id, locale, translation, pageTitle },
+    })
+
+    createPageDependency({ path, nodeId: id })
+  })
+}
+
+// Generate pages from "Page" content type
+async function createSimplePages(graphql, actions, reporter) {
+  const { createPage, createPageDependency } = actions
+  const result = await graphql(`
+    {
+      pages: allContentfulPage {
+        edges {
+          node {
+            id
+            slug
+            node_locale
+            title
+            contentful_id
+            category {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) throw result.errors
+
+  const pageEdges = (result.data.pages || {}).edges || []
+
+  pageEdges.forEach(edge => {
+    const {
+      id,
+      contentful_id,
+      node_locale,
+      slug,
+      category,
+    } = edge.node
+    const genPath = (locale, category, slug) => {
+      const catSlug = category ? category.slug + "/" : ""
+      return `/${locale}/${catSlug}${slug}/`
+    }
+    const path = genPath(node_locale, category, slug)
+    const translationNode = pageEdges.find(
+      p =>
+        p.node.contentful_id === contentful_id &&
+        p.node.node_locale !== node_locale
+    ).node
+    const translation = genPath(
+      translationNode.node_locale,
+      translationNode.category,
+      translationNode.slug
+    )
+    reporter.info(`Creating page: ${path} -> ${translation}`)
+
+    createPage({
+      path,
+      component: require.resolve("./src/templates/simple-page.js"),
+      context: {
+        id: edge.node.id,
+        contentful_id,
+        pageTitle: edge.node.title,
+        translation,
+      },
+    })
+
+    createPageDependency({ path, nodeId: id })
+  })
+}
+
+// Create Contact Pages
+async function createContactPages(actions, reporter) {
+  const { createPage, createPageDependency } = actions
+  const contactPages = [
+    { locale: "en", path: "/en/contact-us/", pageTitle: "Contact Us" },
+    { locale: "es", path: "/es/contactenos/", pageTitle: "Contáctenos" },
+  ]
+  contactPages.forEach(({ path, locale, pageTitle }) => {
+    const translation = contactPages.find(p => p.locale !== locale).path
+    const id = `contact-${locale}`
+    reporter.info(`Creating page: ${path} -> ${translation}`)
+
+    createPage({
+      path,
+      component: require.resolve("./src/templates/contact.js"),
+      context: { id, locale, translation, pageTitle },
+    })
+
+    createPageDependency({ path, nodeId: id })
+  })
+}
+
+// Create product pages
 async function createProductPages(graphql, actions, reporter) {
   const { createPage, createPageDependency } = actions
+  
   const result = await graphql(`
     {
       products: allContentfulProduct {
@@ -19,17 +131,40 @@ async function createProductPages(graphql, actions, reporter) {
 
   const productEdges = (result.data.products || {}).edges || []
 
+  // Generate products index pages
+  const ProductIndexPages = [
+    { locale: "en", path: "/en/products/", pageTitle: "Our Products" },
+    { locale: "es", path: "/es/productos/", pageTitle: "Nuestros Productos" },
+  ]
+  ProductIndexPages.forEach(({ path, locale, pageTitle }) => {
+    const translation = ProductIndexPages.find(p => p.locale !== locale).path
+    reporter.info(`Creating page: ${path} -> ${translation}`)
+    createPage({
+      path,
+      component: require.resolve("./src/templates/list-page.js"),
+      context: {
+        id: `products-${locale}`,
+        locale: locale,
+        pageTitle,
+        translation,
+      },
+    })
+  })
+
   productEdges.forEach(edge => {
     const { id, contentful_id, node_locale, slug } = edge.node
     const prefixes = {
       en: "/en/products/",
-      es: "/es/productos/"
+      es: "/es/productos/",
     }
-    const path = `${prefixes[node_locale]}${slug}`
+    const path = `${prefixes[node_locale]}${slug}/`
     const translationNode = productEdges.find(
-        p => p.node.contentful_id === contentful_id && p.node.node_locale !== node_locale
-      ).node
-    const translation = prefixes[translationNode.node_locale] + translationNode.slug
+      p =>
+        p.node.contentful_id === contentful_id &&
+        p.node.node_locale !== node_locale
+    ).node
+    const translation =
+      prefixes[translationNode.node_locale] + translationNode.slug
     reporter.info(`Creating page: ${path} -> ${translation}`)
 
     createPage({
@@ -42,6 +177,82 @@ async function createProductPages(graphql, actions, reporter) {
   })
 }
 
+// Create brand pages
+async function createBrandPages(graphql, actions, reporter) {
+
+  const { createPage, createPageDependency } = actions
+
+  // Quey brands
+  const result = await graphql(`
+    {
+      brands: allContentfulProductBrand {
+        edges {
+          node {
+            id
+            contentful_id
+            node_locale
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) throw result.errors
+
+  const brandEdges = (result.data.brands || {}).edges || []
+
+  // Generate brands index pages
+  const BrandIndexPages = [
+    { locale: "en", path: "/en/brands/", pageTitle: "Our brands" },
+    { locale: "es", path: "/es/marcas/", pageTitle: "Nuestras marcas" },
+  ]
+  BrandIndexPages.forEach(({ path, locale, pageTitle }) => {
+    const translation = BrandIndexPages.find(p => p.locale !== locale).path
+    reporter.info(`Creating page: ${path} -> ${translation}`)
+    createPage({
+      path,
+      component: require.resolve("./src/templates/list-page.js"),
+      context: {
+        id: `brands-${locale}`,
+        locale: locale,
+        pageTitle,
+        translation,
+      },
+    })
+  })
+
+  // Generate a page for each brand
+  brandEdges.forEach(edge => {
+    const { id, contentful_id, node_locale, slug } = edge.node
+    const prefixes = {
+      en: "/en/brands/",
+      es: "/es/marcas/",
+    }
+    const path = `${prefixes[node_locale]}${slug}/`
+    const translationNode = brandEdges.find(
+      p =>
+        p.node.contentful_id === contentful_id &&
+        p.node.node_locale !== node_locale
+    ).node
+    const translation =
+      prefixes[translationNode.node_locale] + translationNode.slug
+    reporter.info(`Creating page: ${path} -> ${translation}`)
+
+    createPage({
+      path,
+      component: require.resolve("./src/templates/brand.js"),
+      context: { id, contentful_id, locale: node_locale, translation },
+    })
+
+    createPageDependency({ path, nodeId: id })
+  })
+}
+
 exports.createPages = async ({ graphql, actions, reporter }) => {
+  await createHomePages(actions, reporter)
+  await createSimplePages(graphql, actions, reporter)
+  await createContactPages(actions, reporter)
   await createProductPages(graphql, actions, reporter)
+  await createBrandPages(graphql, actions, reporter)
 }
