5199289da5471ba156db0e750e9924d3d469f4af
Guayo Mena <guayo.mena@gmail.com>
2019-06-19 01:31:11 -0600

    Install new google maps component

--- src/components/Location/index.js
+++ src/components/Location/index.js
@@ -1,38 +1,84 @@
-import React from 'react'
-import TitleBar from  '../../components/General/TitleBar'
-import styled from 'styled-components'
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import React, { Component } from "react"
+import GoogleMapReact from "google-map-react"
+import TitleBar from "../../components/General/TitleBar"
+import styled from "styled-components"
+import { MdLocationOn } from "react-icons/md"
 
 const Container = styled.div`
   flex: 1 0;
 `
 
-const latitude = 10.308094191678;
-const longitude = -84.86571288339;
+const MarkerContainer = styled.div`
+  font-size: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  width: 80px;
+  height: 80px;
+  background: rgba(0,0,0,0.2);
+  border-radius: 50%;
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-    <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: latitude, lng: longitude }}
-    >
-        <Marker
-            position={{ lat: 9.9526159, lng: -84.2274045 }}
-        />
-        <Marker
-            position={{ lat: 10.5906306, lng: -85.5347681 }}
-    />
-    </GoogleMap>
-))
+  path {
+    fill: red;
+  }
+`
 
-export default (props) => (
-    <Container>
-        <TitleBar title="Visit our showrooms" />
-        <MyMapComponent
-            isMarkerShown
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `400px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-        />
-    </Container>
+const IconContainer = styled.div`
+  border: solid orange 1px;
+`
+
+const Text = styled.div`
+  width: 100%;
+  font-size: 16px;
+  line-height: 1em;
+  color: #fff;
+`
+
+const AnyReactComponent = ({ text }) => (
+  <MarkerContainer>
+    <div>
+      <MdLocationOn />
+    </div>
+
+    <Text>{text}</Text>
+  </MarkerContainer>
 )
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 9.98,
+      lng: -84.68,
+    },
+    zoom: 8,
+  }
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <Container>
+        <TitleBar title="Visit our showrooms" />
+        <div style={{ height: "400px", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: process.env.GATSBY_GOOGLE_MAPS_KEY }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <AnyReactComponent
+              lat={9.9526159}
+              lng={-84.2274045}
+              text="San José"
+            />
+            <AnyReactComponent
+              lat={10.5906306}
+              lng={-85.5347681}
+              text="Liberia"
+            />
+          </GoogleMapReact>
+        </div>
+      </Container>
+    )
+  }
+}
+
+export default SimpleMap
