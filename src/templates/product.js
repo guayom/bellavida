220dc817eacd167062c8c6f707da8cee6d41de0a
Guayo Mena <guayo.mena@gmail.com>
2019-06-22 18:09:19 -0600

    Add pdf download link to products

--- src/templates/product.js
+++ src/templates/product.js
@@ -5,6 +5,7 @@ import Wrapper from "../components/Layout/Wrapper";
 import InternalHero from "../components/General/InternalHero";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import { FaDownload } from 'react-icons/fa'
 
 const propTypes = {
   data: PropTypes.object.isRequired
@@ -14,9 +15,7 @@ const ProductTemplate = ({ data, pageContext }) => {
   const product = data.contentfulProduct
 
   return (
-    <Layout
-      {...pageContext}
-    >
+    <Layout {...pageContext}>
       <Helmet title={product.title} />
       <InternalHero title={product.title} image={product.image} />
       <Wrapper>
@@ -25,6 +24,26 @@ const ProductTemplate = ({ data, pageContext }) => {
             __html: product.description.childMarkdownRemark.html,
           }}
         />
+        {product.pdf && (
+          <a
+            style={{
+              display: `inline-block`,
+              padding: "8px 20px",
+              background: "#93c548",
+              fontSize: "14px",
+              lineHeight: "14px",
+              color: "#fff",
+              textDecoration: `none`,
+              borderRadius: `4px`,
+              border: 0,
+            }}
+            href={product.pdf.file.url}
+            target="_blank"
+            download={product.pdf.title}
+          >
+            {product.pdf.title} <FaDownload />
+          </a>
+        )}
       </Wrapper>
     </Layout>
   )
@@ -41,6 +60,12 @@ export const pageQuery = graphql`
       description {
         childMarkdownRemark {
           html
+        }
+      }
+      pdf {
+        title
+        file {
+          url
         }
       }
       image {
